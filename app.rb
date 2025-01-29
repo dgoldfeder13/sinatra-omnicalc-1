8dad001b92bdc00d88ac854b5d8f598419538aca
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-01-29 01:31:39 +0000

    still testing

--- app.rb
+++ app.rb
@@ -2,8 +2,40 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+get("/square/new") do
+  erb(:square)
+  
+end
+get("/square/results") do
+  @square_num = params.fetch("number")
+end
+
+
+get("/square_root/new") do
+end
+get("square_root/results") do 
+  @root_numm = params.fetch("number")
+end
+
+
+get("/random/new") do
+end
+get("/random/results") do
+  @min = params.fetch("user_min").to_f
+  @max = params.fetch("user_max").to_f
+
+  @random_num = rand(@min..@max).to_f
+  "test"
+end
+
+get("/payment/new") do
+end
+get("/payment/results") do
+  @apr = params.fetch("user_apr").to_f / 100 
+  @years = params.fetch("user_years").to_i
+  @principal = params.fetch("user_pv").to_f
+  "test"
 end
